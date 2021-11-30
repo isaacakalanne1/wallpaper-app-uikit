0dcd5124213843a6af02aa6dd6695fb246930b52
Isaac Akalanne <iakalann@jaguarlandrover.com>
2021-11-30 15:35:50 +0000

    Created some color constants, and updated the background color of MainViewController

---
+++ Wallpaper App UIKit/Wallpaper App UIKit/Constants/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Wallpaper App UIKit
+//
+//  Created by iakalann on 30/11/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let primary = UIColor(named: "Primary")!
+    static let secondary = UIColor(named: "Secondary")!
+    static let accent = UIColor(named: "Accent")!
+}
