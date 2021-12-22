d58d5ea99e7cfab1e2e7d282c611578dbbd2c0e8
Isaac Akalanne <iakalann@jaguarlandrover.com>
2021-12-22 15:11:25 +0000

    Test commit

--- Wallpaper App UIKit/Wallpaper App UIKit/Feature/Main Screen/Views/TertiaryContainer.swift
+++ Wallpaper App UIKit/Wallpaper App UIKit/Feature/Main Screen/Views/TertiaryContainer.swift
@@ -57,7 +57,6 @@ class TertiaryContainer: UIView {
     }
     
     func displayPermanentAnnouncement(_ announcementText: String?) {
-        
         if let text = announcementText {
             announcementLabel.text = text
             isHidden = false
