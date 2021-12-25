11890cf66736f2d3a5390430dfc0c8472eead8ae
Isaac Akalanne <iakalann@jaguarlandrover.com>
2021-12-25 15:13:01 +0000

    Further to below commit, only targeting iPhone will help with early-stage app release cadence, as will only need to produce screenshots for iPhone

--- Wallpaper App UIKit/Wallpaper App UIKit/AppDelegate.swift
+++ Wallpaper App UIKit/Wallpaper App UIKit/AppDelegate.swift
@@ -11,8 +11,6 @@ import GoogleMobileAds
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
