bf67e47cbe35f89a6c80733cae13f605a203caef
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-01-05 10:23:06 +0000

    Test commit

--- Wallpaper App UIKit/Wallpaper App UIKit/Feature/Main Screen/View Controllers/MainViewController.swift
+++ Wallpaper App UIKit/Wallpaper App UIKit/Feature/Main Screen/View Controllers/MainViewController.swift
@@ -109,7 +109,6 @@ class MainViewController: UIViewController {
         
         loadNewAd()
         
-        
 //        let parameters = UMPRequestParameters()
 //        parameters.tagForUnderAgeOfConsent = false
 //
