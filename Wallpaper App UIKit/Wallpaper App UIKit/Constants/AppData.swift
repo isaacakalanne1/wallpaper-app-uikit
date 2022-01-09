1f46ff64993f6e0ba2e70670d9a681a693c35479
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-01-09 14:40:30 +0000

    Updated app details

--- Wallpaper App UIKit/Wallpaper App UIKit/Constants/AppData.swift
+++ Wallpaper App UIKit/Wallpaper App UIKit/Constants/AppData.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct AppData {
-    static let albumHash = "oxYFZ"
-    static let videoAdId = "ca-app-pub-3940256099942544/1712485313"
+    static let albumHash = "Hle3Utv"
+    static let videoAdId = "ca-app-pub-1852329945819279/1381173930"
 }
